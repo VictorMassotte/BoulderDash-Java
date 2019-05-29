4f4242a0fc2c4cf4272dab09dcaeb951bf7421ed
Victor <massotte.v@gmail.com>
2019-05-29 13:24:12 +0200

    Commit by Victor

--- masterpom/model/src/main/java/Strategy/DiamondStrategy.java
+++ masterpom/model/src/main/java/Strategy/DiamondStrategy.java
@@ -6,8 +6,7 @@ import contract.model.IMobile;
 public class DiamondStrategy extends FallingObjectStrategy {
 
 	public void followStrategy(IMobile currentPawn, IMap map) {
-		if ((currentPawn.getPosition().y == map.getMyCharacter().getPosition().y - 1
-				&& currentPawn.getPosistion().x == map.getMyCharacter().getPosition().x)
+		if ((currentPawn.getPosition().y == map.getMyCharacter().getPosition().y - 1, && currentPawn.getPosistion().x == map.getMyCharacter().getPosition().x)
 				|| currentPawn.getPosistion().equals(map.getMyCharacter().getPosition())) {
 			currentPawn.removeFromBoard();
 			map.decreaseDiamondCount();
