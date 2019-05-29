a0290329ac4347f29865abe1fe9eb688d70e6d45
Valentin <valentin.perrier@viacesi.fr>
2019-05-29 10:02:14 +0200

    Commit by Valentin

--- masterpom/model/src/main/java/factory/Element.java
+++ masterpom/model/src/main/java/factory/Element.java
@@ -7,7 +7,16 @@ import contract.model.Permeability;
 import contract.model.Sprite;
 
 public class Element implements IElement {
+	
+	private Sprite sprite;
+	
+	private Permeability permeability;
 
+	public Element(final Sprite sprite, final Permeability permeability) {
+		this.setSprite(sprite);
+		this.setPermeability(permeability);
+	}
+	
 	@Override
 	public Sprite getSprite() {
 		// TODO Auto-generated method stub
@@ -24,6 +33,14 @@ public class Element implements IElement {
 	public Image getImage() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	public void setPermeability(Permeability permeability) {
+		this.permeability = permeability;
 	}
 
 }
