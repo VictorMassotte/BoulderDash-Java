c3e7e31e42120abad6994aa8ad530be6916ab0a9
VictorMassotte <victor.massotte@viacesi.fr>
2019-06-03 18:26:28 +0200

    Commit by Victor

--- masterpom/main/src/main/java/main/Main.java
+++ masterpom/main/src/main/java/main/Main.java
@@ -11,25 +11,9 @@ import view.Fenetre;
 
 public class Main implements IMain {
 
-	static int TILESIZE = 32;
-	static int WIDTH = 16 * TILESIZE;
-	static int HEIGHT = 16 * TILESIZE;
-	static int OFFSETX = 0;
-	static int OFFSETY = 0;
 	private static Fenetre f = new Fenetre();
 
 	public static void main(final String[] args) throws SQLException, IOException {
-
-		/*
-		 * En fonction du main de Victor Window w = new Window("Boulder dash", WIDTH,
-		 * HEIGHT);
-		 * 
-		 * w.init();
-		 * 
-		 * while (true) { w.update(); w.repaint();
-		 * 
-		 * }
-		 */
 
 		ControllerFenetre c = new ControllerFenetre(f);
 		ControllerMenu m = new ControllerMenu(f);
