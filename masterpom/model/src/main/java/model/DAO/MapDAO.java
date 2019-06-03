41fc87858f2d9e661ab85f973cf5661f88c7d0fa
VictorMassotte <victor.massotte@viacesi.fr>
2019-06-03 18:35:23 +0200

    Commit by Victor

--- masterpom/model/src/main/java/model/DAO/MapDAO.java
+++ masterpom/model/src/main/java/model/DAO/MapDAO.java
@@ -57,7 +57,6 @@ public class MapDAO extends DAO<GameMap> {
                 try {
                     this.resultSet;
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
